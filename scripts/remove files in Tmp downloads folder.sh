8f63ba8f4e935051a8b0b58b69fefbb91a56401d
Viktor Fonic <viktor.fonic@gmail.com>
2012-12-08 15:31:18 +0100

    Added Growl notification when Tmp downloads folder gets emptied

--- scripts/remove files in Tmp downloads folder.sh
+++ scripts/remove files in Tmp downloads folder.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 find ~/Downloads/Tmp\ downloads/ -atime +1d -exec rm {} \;
 find ~/Downloads/Tmp\ downloads/ -depth -empty -delete
+/usr/local/bin/growlnotify -m 'I just emptied Tmp download folder :*'
